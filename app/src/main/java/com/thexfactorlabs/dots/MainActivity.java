576cc11732188d2d86e655fde84b8e6309e076d9
Harry Anuszewski <hanuszewski@probaris.com>
2017-08-07 16:39:04 -0400

    cleaned up imports

--- app/src/main/java/com/thexfactorlabs/dots/MainActivity.java
+++ app/src/main/java/com/thexfactorlabs/dots/MainActivity.java
@@ -1,7 +1,7 @@
 package com.thexfactorlabs.dots;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_main);
         Draw draw = new Draw(this);
         setContentView(draw);
+        //ActionBar titlebar = getActionBar();
+        //if(titlebar != null){
+        //    titlebar.hide();
+        //}
     }
 }
